14d2f3b4b825285081e437bbfb47f5bd77b85410
Jinho <yoonjinho0318@gmail.com>
2019-09-06 12:40:31 +0530

    Complete Assignment #02.

--- 20190906_assignment_02/quadratic.py
+++ 20190906_assignment_02/quadratic.py
@@ -1,3 +1,14 @@
 # Assignment #02
 # Your Name
 # September 6, 2019
+
+print("Quadratic Equation Solver for y = ax^2 + bx + c")
+a = float(input("a: "))
+b = float(input("b: "))
+c = float(input("c: "))
+
+def quadraticFunction(a, b, c): 
+    return (-b + (b ** 2 - 4 * a * c) ** 0.5)/(2 * a)
+
+ans = quadraticFunction(a, b, c)
+print("Answer: " + str(ans))
