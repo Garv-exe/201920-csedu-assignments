ea519828980094878cdc72a9d3f2a1800082b852
Jinho <yoonjinho0318@gmail.com>
2019-09-06 19:00:32 +0530

    Complete Assignment 02.

--- 20190906_assignment_02/quadratic.py
+++ 20190906_assignment_02/quadratic.py
@@ -2,15 +2,16 @@
 # Your Name
 # September 6, 2019
 
+def quadraticFunction(a, b, c): 
+    squared = (b ** 2 - 4 * a * c) ** 0.5
+    # remove duplicate by list(dict.fromkeys())
+    return list(dict.fromkeys([(-b + squared) / (2 * a), (-b - squared) / (2 * a)]))
+
 print("Quadratic Equation Solver for y = ax^2 + bx + c")
 a = float(input("a: "))
 b = float(input("b: "))
 c = float(input("c: "))
 
-def quadraticFunction(a, b, c): 
-    return (-b + (b ** 2 - 4 * a * c) ** 0.5)/(2 * a)
-
 ans = quadraticFunction(a, b, c)
-print("Answer: " + str(ans))
-
-aoeuaoeu
+# *ans iterates over list
+print(*ans, sep=" and ")
