83c3ce69b6565bc942d60e0ec235b8684f06493a
Ritvik Pothapragada <ritvik@Ritviks-MacBook-Pro.local>
2019-09-08 17:30:31 +0530

    Complete Assignment

--- 20190906_assignment_02/quadratic.py
+++ 20190906_assignment_02/quadratic.py
@@ -1,3 +1,19 @@
 # Assignment #02
 # Your Name
 # September 6, 2019
+import math
+print("This program will find the solutions to a quadratic equation using the quadratic formula ")
+a = float(input("Please enter the value of a"))
+b = float(input("Please enter the value of b"))
+c = float(input("Please enter the value of c"))
+squareroot =(b**2)-(4*a*c)
+squareroot = math.sqrt(squareroot)
+finalValueOne= (-b+squareroot)/(2*a)
+finalValueTwo= (-b-squareroot)/(2*a)
+if finalValueOne == finalValueTwo:
+    print("The Solution is: " + str(finalValueOne))
+else:
+    print("One Solution is " + str(finalValueOne))
+    print("Another Solution is " + str(finalValueTwo))
+
+print("Thank you for using my program please rate 5 stars on myspace")
