9af9816407ed29da571b2957b83acf84aa1f21f7
gchan93 <gyungchaehan@woodstock.ac.in>
2019-09-08 15:58:45 +0530

    Assignment #02 Completed.

--- 20190906_assignment_02/quadratic.py
+++ 20190906_assignment_02/quadratic.py
@@ -1,3 +1,14 @@
 # Assignment #02
-# Your Name
+# Gyungchae Han
 # September 6, 2019
+
+print("Welcome to Quadratic Solver!")
+
+a = int(input("Please enter value for a = "))
+b = int(input("Please enter value for b = "))
+c = int(input("Please enter value for c = "))
+
+x1 = (-b + (b**2 - 4*a*c)**(1/2))/(2*a)
+x2 = (-b - (b**2 - 4*a*c)**(1/2))/(2*a)
+
+print("x = " + str(x1))if x1 == x2 else print("x = " + str(x1) + " and x = " + str(x2))
