3c81bfc7669cd88295fc3d209387c3435916f1ec
Doaa Althawr <doaaalthawr@woodstock.ac.in>
2019-09-12 20:07:26 +0530

    Complete Assignment 02.

--- 20190906_assignment_02/quadratic.py
+++ 20190906_assignment_02/quadratic.py
@@ -1,3 +1,16 @@
-# Assignment #02
-# Your Name
-# September 6, 2019
+#Assignmnet 2
+#Doaa Althawr
+#8/9/2019
+print ("solve for the quadratic functions")
+a = float (input("enter the value for a:"))
+b = float (input ("enter the value for b:"))
+c = float (input ("enetr the value for c:"))
+
+
+Value1 = (-b+(b**2 - 4*a*c)**0.5) /(2*a)
+Value2 = (-b-(b**2 - 4*a*c)**0.5) /(2*a)
+
+
+print (Value1)
+print (Value2)
+
